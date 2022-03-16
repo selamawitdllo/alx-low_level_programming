4112f98870128a9944c14bfee0ee66003fd77dd4
selamawitdllo <selam.dlloa@gmail.com>
2022-03-16 23:02:54 +0300

    function nest

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *	alphabet -10 times of alphabet,alphabet order
+ *	Description:the code with 10 times print
+ *	alphabetics order
+ *
+ */
+int _print_alphabetx10()
+{
+printf("%s\n","abcdefghijklmnopqrstuvwxyz")
+}
+int main (void)
+{
+print_alphabet_x10();
+return (0);
