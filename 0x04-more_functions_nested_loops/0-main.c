387383c4ee24e0905c90ad749f472d64470d871e
selamawitdllo <selam.dlloa@gmail.com>
2022-03-17 12:17:48 -0700

    low

---
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *      main - check the code.
+ *
+ *      Return: Always 0.
+ */
+int _isupper(int c)
+{
+    char c;
+    if ( c = 'A' );
+    {
+    printf("%c: %d\n", c, _isupper(c));
+    }
+    else ( c = 'a' );
+    {
+    printf("%c: %d\n", c, _isupper(c));
+    }
+    return (0);
+}
+
