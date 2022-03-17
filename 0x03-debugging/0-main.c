acfcfe2a24309fbf44f2f6c943ca3c845d62885f
selamawitdllo <selam.dlloa@gmail.com>
2022-03-17 03:30:34 -0700

    positive

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,22 @@
+#include "main,h"
+#include <stdio.h>
+
+/**
+ *	main - tests function that if print intrger
+ *
+ *	return: 0
+ *
+ */
+int main(void)
+{
+	int i;
+	if(i >= 0)
+{
+	printf("%s\n","positive");
+}
+	else
+{
+	printf("%s\n","negative")
+}
+	return (0);
+}
