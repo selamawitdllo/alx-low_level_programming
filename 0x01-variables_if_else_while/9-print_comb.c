73db113c1519f0705d4c14c9c8284a98fdb6424b
selamawitdllo <selam.dlloa@gmail.com>
2022-03-16 20:52:43 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - function main
+ *
+ * Discription: this is main function
+ *
+ * Return: 0 if success, 1 otherwise
+ */
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n);
+		if (n < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
