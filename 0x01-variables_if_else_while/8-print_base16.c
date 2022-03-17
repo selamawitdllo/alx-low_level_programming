9f12b9c68101a0087068691a83c4c1589f1f0798
selamawitdllo <selam.dlloa@gmail.com>
2022-03-16 20:48:11 -0700

    hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - function main
+ *
+ * Discription: this is main function
+ *
+ * Return: 0 if success, 1 otherwise
+ */
+int main(void)
+{
+	int n = 0;
+	int d = 97;
+	for (n = 0; n <= 15; n++)
+	{
+		if (n < 10)
+			putchar(n + 48);
+		else
+		{	
+			putchar(d++);
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
