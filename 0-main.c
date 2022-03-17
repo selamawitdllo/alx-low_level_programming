da928e4008c8611e4456bf18a4b14f2107c08131
selamawitdllo <selam.dlloa@gmail.com>
2022-03-17 02:53:41 -0700

    positive

---
+++ 0-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+include <stdio.h>
+/**
+ *	main -tests function that prints if integer
+ *
+ *	return: 0
+ *
+ *
+ */
+int main(void)
+{
+	int i;
+	if(i >= 0)
+{
+	printf("%s\n","positve");
+}
+	else
+{
+	printf("%s\n","negative");
+}
+}
+	return (0);
---
+++ 2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stido.h>
+
+/*
+ *	op_print - makes 10 times
+ *	Description:make to print a to z
+ *	in more times
+ *
+ */
+int print_alphabet_10();
+{
+printf("%s\n","abcdefghijklmnopqrstuvwxyz")
+}
+int main(void)
+{
+print_alphabet_10();
+return (0);
