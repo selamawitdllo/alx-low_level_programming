38e994f3a96cb0ebf40b02f6f254ab6a0ee2ce75
selamawitdllo <selam.dlloa@gmail.com>
2022-03-16 19:50:55 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints puts
+ *
+ * Description: This is the main function
+ * and it prints using puts
+ *
+ * Return: 0 if correct, 1 othewise
+ */
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
