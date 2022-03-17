1f34437dc11885b35518c952794389e8d80cb88d
selamawitdllo <selam.dlloa@gmail.com>
2022-03-17 00:40:50 -0700

    return

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ *	int _islower(int c) -check the code
+ *
+ *	Description: check the c islower case or not
+ */
+int _islower(int c)
+{	
+	
+	if(c==islower)
+	return (1);
+}
+int main()
+{
+	int c;
+	return (0);
+}
