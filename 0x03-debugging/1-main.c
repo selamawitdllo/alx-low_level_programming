905e69a92e227025b14acd98874dc7432026e21f
selamawitdllo <selam.dlloa@gmail.com>
2022-03-17 10:45:05 -0700

    main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-	int i;
+        int i;
 
 	printf("Infinite loop incoming :(\n");
 
@@ -17,6 +17,8 @@ int main(void)
 *	{
 *	putchar(i);
 */	}
+
 	printf("Infinite loop avoided! \\o/\n");
-	return (0);
+
+        return (0);
 }
