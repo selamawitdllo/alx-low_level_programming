cddeff3eaea99122a6a1ebb97a62c704b86841d4
selamawitdllo <selam.dlloa@gmail.com>
2022-03-16 19:56:58 -0700

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints puts
+ *
+ * Description: This is the main function
+ * and it prints using puts
+ *
+ * Return: 0 if correct, 1 othewise
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n",sizeof(char));
+	printf("Size of a int: %d byte(s)\n",sizeof(int));
+	printf("Size of a long int: %d byte(s)\n",sizeof(long int));
+	printf("Size of a long long: %d byte(s)\n",sizeof(long long));
+	printf("Size of a float: %d byte(s)\n",sizeof(float));
+	return (0);
+}
